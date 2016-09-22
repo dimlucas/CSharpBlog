1eec7d7f4507499bc8167fe5ebb3b6e0d668e356
Dimitris Loukas <dimlucas@outlook.com>
2016-09-22 23:12:14 +0300

    Added ObservableObject base class

---
+++ ObservableObjectDemo/Models/ObservableObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableObjectDemo.Models
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void setValue<T>(string propertyName, ref T property, T value)
+        {
+            //Set the property and raise the event only if the property's value is actually changed
+            if (!property.Equals(value))
+            {
+                property = value;
+                raisePropertyChanged(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// Raises the "PropertyChanged" event for the given property
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed</param>
+        protected void raisePropertyChanged(string propertyName)
+        {
+            //First check if there are any methods subscribed in the event
+            if(PropertyChanged != null)
+            {
+                //Then construct the event's arguments
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+                //Raise the event
+                PropertyChanged(this, args);
+            }
+        }
+    }
+}
