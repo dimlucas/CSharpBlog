ea2372455940446f82f0e9f49adb02612e81887e
Dimitris Loukas <dimlucas@outlook.com>
2017-05-06 15:59:56 +0300

    Overriding the indexer operator tutorial

---
+++ IndexerOperator/Garage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerOperator
+{
+    public class Garage
+    {
+        
+        /// <summary>
+        /// Adds a car to the garage
+        /// </summary>
+        /// <param name="car">The car to be added to the garage</param>
+        public void Add(Car car)
+        {
+            //Searches the list for any occurrences of the given car
+            if (cars.FindAll((c) => { return c.Equals(car); }).Count == 0)
+            {
+                cars.Add(car);
+            }
+        }
+
+        /// <summary>
+        /// Returns the car at the specified index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public Car Get(int index)
+        {
+            return cars.ElementAt(index);
+        }
+
+        /// <summary>
+        /// Removes a car from the garage
+        /// </summary>
+        /// <param name="car">The instance of the car to be removed from the garage</param>
+        public void Remove(Car car)
+        {
+            cars.Remove(car);
+        }
+
+        /// <summary>
+        /// The number of cars in the garage
+        /// </summary>
+        public int Count
+        {
+            get { return cars.Count; }
+        }
+
+        public void Clean()
+        {
+            foreach(var car in cars)
+            {
+                car.Clean = true;
+            }
+        }
+
+        public void Repair()
+        {
+            foreach(var car in cars)
+            {
+                car.DamageAmount = 0;
+            }
+        }
+
+        public Car this[int index]
+        {
+            get
+            {
+                return Get(index);
+            }
+        }
+
+        protected List<Car> cars = new List<Car>();
+    }
+}
---
+++ IndexerOperator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            testGarage();
+            Console.ReadKey();
+        }
+
+        private static void testGarage()
+        {
+            Garage garage = new Garage();
+            garage.Add(new Car()
+            {
+                Make = "Porsche",
+                Model = "Carrera GT",
+                Year = 2011,
+                Clean = true,
+                DamageAmount = 27
+            });
+            garage.Add(new Car()
+            {
+                Make = "Toyota",
+                Model = "Yaris",
+                Year = 2015,
+                Clean = true,
+                DamageAmount = 0
+            });
+            garage.Add(new Car()
+            {
+                Make = "Ford",
+                Model = "Mustang GT",
+                Year = 2010,
+                Clean = false,
+                DamageAmount = 68
+            });
+            Car cityCar = garage[1];
+            Console.WriteLine(String.Format("{0} {1}", cityCar.Make, cityCar.Model));
+        }
+    }
+}
